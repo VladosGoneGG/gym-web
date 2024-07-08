6d6f2d767384bbcb47d867207b611fdaa4b9922b
Vladislav Medelean <vlad93gm@gmail.com>
2024-07-08 14:30:04 +0300

    ver 1.1

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { AuthResponse } from '../models/response/AuthResponse'
 
-export const API_URL = 'http://localhost:5000/api'
+export const API_URL = 'https://gym-fitform.com/api'
 
 const $api = axios.create({
 	withCredentials: true,
